f4a3cf40c6ae96aa031dadf249acd3fd2141cb53
root <root@localhost.localdomain>
2019-07-20 13:22:33 +0800

    VariadicTemplatesDemo

---
+++ variadicTemplates.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+// We should define this empty print function in order to avoid compilation failure.
+void print()
+{
+}
+
+template <typename T, typename... Types>
+void print(const T& firstArg, const Types&... args)
+{
+    cout << firstArg << endl;
+    print(args...);
+}
+
+int main()
+{
+    print(7.5, "Hello", bitset<16>(377), 42);
+
+    return 0;
+}
